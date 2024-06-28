f70bf3704ea24330255573534cb934cbe2b0d9b7
Venic <sashabratchikov72@gmail.com>
2024-06-28 11:02:27 +0300

    program cs, DbConnection

--- Project.WebAPI/Program.cs
+++ Project.WebAPI/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-app.MapGet("/", () => "Hello World!");
+using System;
 
-app.Run();
+using Project.Persistence;
+
+namespace Project.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<ProjectsDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception exception)
+                {
+                    
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
