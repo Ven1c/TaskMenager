3f36a3f35c570315abacd59785e6cebc2a0c685c
Venic <sashabratchikov72@gmail.com>
2024-07-05 03:51:04 +0300

    make first request and handler with MediatR

---
+++ Project.Application/Projects/Commands/CreateProject/CreateProjectCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Project.Application.Projects.Commands.CreateProject
+{
+    public class CreateProjectCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public enum StatusCombination { New, Closed }
+    }
+}
---
+++ Project.Application/Projects/Commands/CreateProject/CreateProjectCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Project.Application.Interfaces;
+using Projects.Domain;
+
+namespace Project.Application.Projects.Commands.CreateProject
+{
+    public class CreateProjectCommandHandler
+        :IRequestHandler<CreateProjectCommand, Guid>
+    {
+        private readonly IProjectDbContext _projectDbContext;
+        public CreateProjectCommandHandler(IProjectDbContext dbContext) =>
+            _projectDbContext = dbContext;
+        public async Task<Guid> Handle(CreateProjectCommand request,CancellationToken cancellationToken)
+        {
+            var project = new Project_
+            {
+                Name = request.Name,
+                Details = request.Details,
+                AuthorId = request.AuthorId,
+                Id = Guid.NewGuid()
+                //status combination
+            };
+            await _projectDbContext.Projects_.AddAsync(project,cancellationToken);
+            await _projectDbContext.SaveChangesAsync(cancellationToken);
+            return project.Id;
+        }
+    }
+}
